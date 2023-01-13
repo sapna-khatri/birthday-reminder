2364d6012cf3c0b3cec53da27cf40c2ad3ecb48c
sapna-khatri <sapnafbkhatri@gmail.com>
2023-01-13 16:10:45 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: 'Sapna Khatri',
+        age: 24,
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+        id: 2,
+        name: 'Aakash Mali',
+        age: 24,
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+        id: 3,
+        name: 'Hrishikesh Mane',
+        age: 24,
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+        id: 4,
+        name: 'Kartik Patil',
+        age: 24,
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+        id: 5,
+        name: 'Abhijit Khatri',
+        age: 24,
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+];
